20fb2e9633bd877252f1c5a23c44d0cc8bbf2a95
Edale Miguel <edale.miguel@seattlecolleges.edu>
2025-01-17 23:56:52 -0800

    Moved files to root for Github Pages

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function exploreFeatured() {
   // Rotating Banner Carousel
   let currentImageIndex = 0;
   const images = [
-    './images/EarlyBloom.jpg',
-    './images/LanternInTheDark.jpg',
-    './images/PoolBalls.jpg',
-    './images/Envy.jpg'
+    '/images/EarlyBloom.jpg',
+    '/images/LanternInTheDark.jpg',
+    '/images/PoolBalls.jpg',
+    '/images/Envy.jpg'
   ];
   
   function rotateBanner() {
